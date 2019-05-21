b9ee7fd3a5ab63991f69dd3b0330d175303eb138
Alexandre <0x41e@protonmail.ch>
2019-05-21 00:30:48 -0300

    Add crawlers models
    This model works a hub for interface and contract. It defines the
    input and output to the API endpoint (contract) and also defines what
    every crawler should consider as the process representation (interface).
    
    * In some points it defines '# type: ignore' to avoid issues between
    pydantic and mypy.

---
+++ jus_brasil/crawlers/models.py
@@ -0,0 +1,69 @@
+import enum
+import typing as t
+
+from pydantic import BaseModel, Schema
+
+
+# Duplicated for now
+RE_UNIFIED_YEAR_AND_JUS = r"^(?P<unif_year>[0-9]{7}\-[0-9]{2}\.[0-9]{4})\.[0-9]\.[0-9]{2}\.(?P<jurisdiction>[0-9]{4})$"  # noqa
+
+
+class KnownCourts(str, enum.Enum):
+    tjsp = "TJSP"
+    tjms = "TJMS"
+
+
+class Court(BaseModel):
+    initials: KnownCourts
+
+
+class Part(BaseModel):
+
+    role: str
+    identification: str
+
+
+class Judge(BaseModel):
+    name: str
+
+
+class Action(BaseModel):
+    value: str
+
+
+class Activity(BaseModel):
+    date: str
+    activity: str
+
+
+class Process(BaseModel):
+
+    number: str
+    class_: str
+    area: str
+    subject: str
+    subject_details: str
+    distribution: str
+    control: str
+    judge: Judge
+    action: Action
+    parts: t.List[Part]
+    activities: t.List[Activity]
+
+    class Config:
+        fields = {"class_": "class"}
+
+
+class QueryInput(BaseModel):
+
+    process_number: str = Schema(  # type: ignore
+        ..., title="O número do processo formatado", regex=RE_UNIFIED_YEAR_AND_JUS
+    )
+
+    process_court: KnownCourts = Schema(  # type: ignore
+        ..., title="O tribunal relacionado"
+    )
+
+
+class QueryOutPut(Process):
+    pass
