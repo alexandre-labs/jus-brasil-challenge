0c0f9eb429b8405b00138da3e25f2f3a1ab6c3f2
Alexandre <0x41e@protonmail.ch>
2019-05-24 20:47:59 -0300

    Fix object name

--- jus_brasil/api/query.py
+++ jus_brasil/api/query.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 
 from jus_brasil.crawlers import execute_crawler
-from jus_brasil.crawlers.models import QueryInput, QueryOutPut
+from jus_brasil.crawlers.models import QueryInput, QueryOutput
 
 
 router = APIRouter()
 
 
-@router.post("/query", response_model=QueryOutPut)
-async def query_process_data(query_input: QueryInput):
+@router.post("/query", response_model=QueryOutput)
+async def query_process_data(query_input: QueryInput) -> QueryOutput:
     return await execute_crawler(query_input)
