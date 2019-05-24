5304e915af7b9e2735f453c2f764c806f29eff0b
Alexandre <0x41e@protonmail.ch>
2019-05-24 20:24:44 -0300

    Move module downloads.py to utils.py

--- jus_brasil/crawlers/utils.py
+++ jus_brasil/crawlers/utils.py
@@ -3,6 +3,9 @@ import typing as t
 
 import aiohttp
 
+from jus_brasil import config
+from .models import KnownCourts
+
 
 RE_UNIFIED_YEAR_AND_JUS = r"^(?P<unif_year>[0-9]{7}\-[0-9]{2}\.[0-9]{4})\.[0-9]\.[0-9]{2}\.(?P<jurisdiction>[0-9]{4})$"  # noqa
 
@@ -18,19 +21,6 @@ async def _extract_unified_year_and_jurisdiction(
     return match_strings.groups()
 
 
-async def build_query_url(base_url: str, process_number: str) -> str:
-
-    unif_year, jurisdiction = await _extract_unified_year_and_jurisdiction(process_number)
-
-    return (
-        f"{base_url}?conversationId=&dadosConsulta.localPesquisa.cdLocal=-1&"
-        f"cbPesquisa=NUMPROC&dadosConsulta.tipoNuProcesso=UNIFICADO&"
-        f"numeroDigitoAnoUnificado={unif_year}&foroNumeroUnificado={jurisdiction}&"
-        f"dadosConsulta.valorConsultaNuUnificado={process_number}&"
-        f"dadosConsulta.valorConsulta=&uuidCaptcha="
-    )
-
-
 async def get_process_page(session: aiohttp.ClientSession, process_url: str):
     async with session.get(process_url, ssl=False) as response:
         return await response.text()
@@ -39,3 +29,18 @@ async def get_process_page(session: aiohttp.ClientSession, process_url: str):
 async def download_process_page(process_url: str):
     async with aiohttp.ClientSession() as session:
         return await get_process_page(session, process_url)
+
+
+async def get_first_jurisdiction(known_court: KnownCourts) -> str:
+    return config.TJSP_PG if known_court == KnownCourts.tjsp else config.TJMS_PG
+
+
+async def get_second_jurisdiction(known_court: KnownCourts) -> str:
+    return config.TJSP_SG if known_court == KnownCourts.tjsp else config.TJMS_SG
+
+
+async def get_second_jurisdiction_appeal(known_court: KnownCourts) -> t.Optional[str]:
+
+    if known_court == KnownCourts.tjsp:
+        return config.TJSP_SG_REC
+    return None
