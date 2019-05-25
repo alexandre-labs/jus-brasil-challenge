22dd189a22311f2ca1067af6600f816884a66f43
Alexandre <0x41e@protonmail.ch>
2019-05-25 16:26:25 -0300

    Add integration tests

---
+++ tests/integration/crawlers/test_utils.py
@@ -0,0 +1,27 @@
+import aiohttp
+import pytest
+
+from jus_brasil.crawlers import download
+
+
+@pytest.mark.asyncio
+async def test_get_process_page(aresponses):
+
+    query_url = "https://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=7J0001D040000"
+    aresponses.add("esaj.tjsp.jus.br", response="some weird html", match_querystring=True)
+
+    async with aiohttp.ClientSession() as session:
+        process_page = await download.get_process_page(session, query_url)
+
+    assert process_page == "some weird html"
+
+
+@pytest.mark.asyncio
+async def test_download_page(aresponses):
+
+    aresponses.add("esaj.tjsp.jus.br", response="some weird html", match_querystring=True)
+
+    query_url = "https://esaj.tjsp.jus.br/cpopg/show.do?processo.codigo=7J0001D040000"
+    process_page = await download.download_process_page(query_url)
+
+    assert process_page == "some weird html"
