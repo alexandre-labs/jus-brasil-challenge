90dd2b6e9a370121d44567beaa2719091004b99c
Alexandre <0x41e@protonmail.ch>
2019-05-25 10:11:17 -0300

    Fix function return and refactoring
    * Create one small function per jurisdiction
    * Fix the return where a List[Optional[Process]] is expected
    
    TODO: Adding more type hints to check it

--- jus_brasil/crawlers/__init__.py
+++ jus_brasil/crawlers/__init__.py
@@ -1,32 +1,40 @@
 from jus_brasil.crawlers import cpopg, cposg, utils
-from jus_brasil.crawlers.models import KnownCourts, QueryInput, QueryOutput
+from jus_brasil.crawlers.models import QueryInput, QueryOutput
 
 
-async def get_all_urls(process_court: KnownCourts):
-    return (
-        await utils.get_first_jurisdiction(process_court),
-        await utils.get_second_jurisdiction(process_court),
-        await utils.get_second_jurisdiction_appeal(process_court),
+async def execute_crawler_first_jurisdiction(query_input: QueryInput):
+
+    jurisdiction_url = await utils.get_first_jurisdiction(query_input.process_court)
+    return await cpopg.execute_query(jurisdiction_url, query_input)
+
+
+async def execute_crawler_second_jurisdiction(query_input: QueryInput):
+
+    jurisdiction_url = await utils.get_second_jurisdiction(query_input.process_court)
+    return await cposg.execute_query(jurisdiction_url, query_input)
+
+
+async def execute_crawler_second_appeal_jurisdiction(query_input: QueryInput):
+
+    jurisdiction_url = await utils.get_second_appeal_jurisdiction(
+        query_input.process_court
     )
+
+    if jurisdiction_url:
+        return await cposg.execute_query(jurisdiction_url, query_input)
+    return []
 
 
 async def execute_crawler(query_input: QueryInput) -> QueryOutput:
 
-    first, second, second_appeal = await get_all_urls(query_input.process_court)
-
-    first_juristiction = await cpopg.execute_query(first, query_input)
-
-    second_juristiction = await cposg.execute_query(second, query_input)
-
-    if second_appeal:
-        second_jurisdiction_appeal = await cposg.execute_query(
-            second_appeal, query_input
-        )
-    else:
-        second_jurisdiction_appeal = None
+    first_juristiction = await execute_crawler_first_jurisdiction(query_input)
+    second_juristiction = await execute_crawler_second_jurisdiction(query_input)
+    second_jurisdiction_appeal = (
+        await execute_crawler_second_appeal_jurisdiction(query_input)
+    )
 
     return QueryOutput(
         first_juristiction=first_juristiction,
         second_juristiction=second_juristiction,
-        second_jurisdiction_appeal=second_jurisdiction_appeal,
+        second_jurisdiction_appeal=second_jurisdiction_appeal
     )
